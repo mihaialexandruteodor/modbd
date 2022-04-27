d3ed6acf00fc68ac76d26df1a62311b9f4532d3d
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-04-27 14:49:27 +0300

    progress

--- modbd/src/main/java/com/modbd/modbd/ModbdApplication.java
+++ modbd/src/main/java/com/modbd/modbd/ModbdApplication.java
@@ -3,8 +3,12 @@ package com.modbd.modbd;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ModbdApplication  implements CommandLineRunner {
 
@@ -17,7 +21,7 @@ public class ModbdApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String sql = "SELECT * FROM Students";
+		//String sql = "SELECT * FROM Students";
 
 		//List<Student> students = jdbcTemplate.query(sql,
 		//		BeanPropertyRowMapper.newInstance(Student.class));
---
+++ modbd/src/main/java/com/modbd/modbd/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.modbd.modbd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/"})
+    public String index(Model model)
+    {
+        return "index";
+    }
+}
